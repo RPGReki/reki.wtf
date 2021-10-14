2a399c2c8d0062161607f31221a16958fbc55b41
Thai Chung <mail@0xreki.de>
2021-10-14 09:21:53 +0200

    Move back to netlify

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,12 @@ var buildSrc = "./";
 gulp.task("get-comments", function (done) {
 
     // set up the request with appropriate auth token and Form ID
-    var url = `https://gitlab.com/api/v4/projects/27243899/issues/3/notes?private_token=${process.env.GITLAB_TOKEN}&per_page=100`;
-
+    var url = `https://api.netlify.com/api/v1/forms/${process.env.COMMENT_FORM_ID}/submissions/?access_token=${process.env.API_AUTH}`;
     var comments = {};
 
     fs.readFile(buildSrc + "site/_data/saved-comments.json", 'utf8', (err, data) => {
         comments = JSON.parse(data);
     });
-
     // get the data from Netlify's submissions API
     request(url, function(err, response, body){
         if(!err && response.statusCode === 200){
@@ -27,26 +25,23 @@ gulp.task("get-comments", function (done) {
 
             // shape the data
             for(var item in body){
-                try {
-                    var data = JSON.parse(body[item].body);
+                var data = body[item].data;
 
-                    var comment = {
-                        slug: data.slug,
-                        date: body[item].created_at,
-                        name: data.name,
-                        url: data.url,
-                        gravatar: md5(data.email),
-                        message: data.message
-                    };
+                var comment = {
+                    name: data.name,
+                    url: data.url,
+                    gravatar: md5(data.email),
+                    message: data.message,
+                    slug: data.slug,
+                    date: body[item].created_at,
+                    ip: data.ip
+                };
 
-                    // Add it to an existing array or create a new one
-                    if(comments[data.slug]){
-                        comments[data.slug].push(comment);
-                    } else {
-                        comments[data.slug] = [comment];
-                    }
-                } catch (e) {
-                    continue;
+                // Add it to an existing array or create a new one
+                if(comments[data.slug]){
+                    comments[data.slug].push(comment);
+                } else {
+                    comments[data.slug] = [comment];
                 }
             }
 
@@ -63,6 +58,92 @@ gulp.task("get-comments", function (done) {
 
         } else {
             console.log("Couldn't get comments from Netlify");
+            done();
+        }
+    });
+});
+
+gulp.task("get-poll-referral", function (done) {
+
+    // set up the request with appropriate auth token and Form ID
+    var url = `https://api.netlify.com/api/v1/forms/${process.env.POLL_ID_REFERRAL}/submissions/?access_token=${process.env.API_AUTH}`;
+
+    // get the data from Netlify's submissions API
+    request(url, function(err, response, body){
+        if(!err && response.statusCode === 200){
+            argv.silent || console.log("Submissions found");
+            var body = JSON.parse(body);
+            var poll = [];
+
+            // shape the data
+            for(var item in body){
+                var data = body[item].data;
+
+                var vote = {
+                    referrer: data.vote,
+                    date: body[item].created_at,
+                    ip: data.ip
+                };
+
+                poll.push(vote);
+            }
+
+            // write our data to a file where Jekyll can get it.
+            fs.writeFile(buildSrc + "site/_data/polls/referral.json", JSON.stringify(poll, null, 2), function(err) {
+                if(err) {
+                    console.log(err);
+                    done();
+                } else {
+                    argv.silent || console.log("Poll data saved.");
+                    done();
+                }
+            });
+
+        } else {
+            console.log("Couldn't get poll from Netlify");
+            done();
+        }
+    });
+});
+
+gulp.task("get-poll-audio-2021", function (done) {
+
+    // set up the request with appropriate auth token and Form ID
+    var url = `https://api.netlify.com/api/v1/forms/${process.env.POLL_ID_AUDIO_2021}/submissions/?access_token=${process.env.API_AUTH}`;
+
+    // get the data from Netlify's submissions API
+    request(url, function(err, response, body){
+        if(!err && response.statusCode === 200){
+            argv.silent || console.log("Submissions found");
+            var body = JSON.parse(body);
+            var poll = [];
+
+            // shape the data
+            for(var item in body){
+                var data = body[item].data;
+
+                var vote = {
+                    referrer: data.vote,
+                    date: body[item].created_at,
+                    ip: data.ip
+                };
+
+                poll.push(vote);
+            }
+
+            // write our data to a file where Jekyll can get it.
+            fs.writeFile(buildSrc + "site/_data/polls/audio-2021.json", JSON.stringify(poll, null, 2), function(err) {
+                if(err) {
+                    console.log(err);
+                    done();
+                } else {
+                    argv.silent || console.log("Poll data saved.");
+                    done();
+                }
+            });
+
+        } else {
+            console.log("Couldn't get poll from Netlify");
             done();
         }
     });
