c9df7ac0adbdccc196ce0773dbc63fc8f2519be4
Thai Chung <mail@0xreki.de>
2022-06-02 11:48:43 +0200

    2022-06-02T11:48:43 CEST

--- site/.dev/js/base.js
+++ site/.dev/js/base.js
@@ -39,6 +39,15 @@ async function setUpPageForUsers() {
   
   d.addEventListener('beforeprint', ()=>{
     $('img').prop('loading','eager')
+  });
+
+  d.addEventListener('copy', (e) => {
+    const s = d.getSelection();
+    const t = d.createElement('div');
+    const l = $('[rel=shortlink]')[0].href || d.URL;
+    for(var i=0; i < s.rangeCount; ++i) t.appendChild(s.getRangeAt(i).cloneContents());
+    e.clipboardData.setData('text/html', '<link rel="canonical" href="' + l + '">' + t.innerHTML + '<p style="text-align:right">' + $('#copyright p')[0].innerHTML + '<br><a href="' + l + '" rel="canonical">' + l + '</a></p>');
+    e.preventDefault()
   })
 }
 
