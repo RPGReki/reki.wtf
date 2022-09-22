3018c320b313786a04ac8650fe048424e9108785
Thai Chung <mail@0xreki.de>
2022-09-22 16:56:15 +0200

    Add Service Worker

---
+++ site/worker.js
@@ -0,0 +1,7 @@
+importScripts('/assets/2021/scripts/workbox/workbox-sw.js');
+self.addEventListener("message",(event)=>{if(event.data&&event.data.type==="SKIP_WAITING")self.skipWaiting()});
+const now=new Date(Date.now());
+const refresh=Date.UTC(now.getUTCFullYear(),now.getUTCMonth(),now.getUTCDay()+1,03,11);
+const retentionTime=refresh-now;
+const bgSync=new workbox.backgroundSync.BackgroundSyncPlugin("bg-sync-queue", {maxRetentionTime: retentionTime.valueOf()})
+workbox.routing.registerRoute(new RegExp('/*'),new workbox.strategies.StaleWhileRevalidate({cacheName:"offline-cache"}))
